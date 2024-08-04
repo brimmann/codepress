b51d3220a6fe74229c6b8edbccf8faf05ccedc51
Rashid <rashidstanikzai.net@gmail.com>
2024-08-04 22:16:54 +0500

    initial setup worked

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,22 @@
 import { createServer, Plugin } from 'vite'
-import path from 'path'
 import { promises as fs } from 'fs'
+import path from 'path'
 
 const indexTemplate = fs.readFile(path.join(__dirname, '../theme/index.html'))
 
-const VitePressPlugin: Plugin = ({ root, app }) => {
+const CodePressPlugin: Plugin = ({ root, app }) => {
   app.use(async (ctx, next) => {
-    // redirect request to index.html
     if (ctx.path === '/index.html') {
       ctx.body = await indexTemplate
       return
     }
-
     return next()
   })
-
   app.use(async (ctx, next) => {})
 }
 
 createServer({
-  plugins: [VitePressPlugin]
+  plugins: [CodePressPlugin]
 }).listen(3000, () => {
   console.log('listening on http://localhost:3000')
 })
