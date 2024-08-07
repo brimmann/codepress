af0ae6f09fc0326bcd8d7a774a51c827c8538ebc
Rashid <rashidstanikzai.net@gmail.com>
2024-08-07 15:23:10 +0500

    feat: spa navigation added

--- lib/theme-default/Layout.vue
+++ lib/theme-default/Layout.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="theme-container">
+    <ul>
+      <li><a href="/">index</a></li>
+      <li><a href="/test2">test2</a></li>
+      <li><a href="/topic1/top">top</a></li>
+    </ul>
+    
+    
+    
     <Content />
   </div>
 </template>
